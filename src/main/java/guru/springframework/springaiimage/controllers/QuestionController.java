d94f263d0a9725f821d36ac7989e5415e50ea221
Gábor <gabor.roszjar@gmail.com>
2024-10-04 16:37:48 +0200

    2 - usage of dall-e

--- src/main/java/guru/springframework/springaiimage/controllers/QuestionController.java
+++ src/main/java/guru/springframework/springaiimage/controllers/QuestionController.java
@@ -5,6 +5,7 @@ import guru.springframework.springaiimage.services.OpenAIService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -17,7 +18,7 @@ public class QuestionController {
     private final OpenAIService openAIService;
 
     @PostMapping(value = "/image", produces = MediaType.IMAGE_PNG_VALUE)
-    public byte[] getImage(String question) {
+    public byte[] getImage(@RequestBody String question) {
         return openAIService.getImage(new Question(question));
     }
 }
--- src/main/java/guru/springframework/springaiimage/services/OpenAIServiceImpl.java
+++ src/main/java/guru/springframework/springaiimage/services/OpenAIServiceImpl.java
@@ -2,7 +2,14 @@ package guru.springframework.springaiimage.services;
 
 import guru.springframework.springaiimage.model.Question;
 import lombok.RequiredArgsConstructor;
+import org.springframework.ai.image.ImageClient;
+import org.springframework.ai.image.ImageOptionsBuilder;
+import org.springframework.ai.image.ImagePrompt;
+import org.springframework.ai.openai.OpenAiImageClient;
+import org.springframework.ai.openai.OpenAiImageOptions;
 import org.springframework.stereotype.Service;
+
+import java.util.Base64;
 
 /**
  * Created by jt, Spring Framework Guru.
@@ -11,9 +18,22 @@ import org.springframework.stereotype.Service;
 @Service
 public class OpenAIServiceImpl implements OpenAIService {
 
+    final OpenAiImageClient imageClient;
+
     @Override
     public byte[] getImage(Question question) {
+        var options = OpenAiImageOptions.builder()
+                .withHeight(1024).withWidth(1024)
+                .withResponseFormat("b64_json")
+                .withModel("dall-e-3")
+                .withQuality("hd")  //default az a standard
+                .withStyle("natural")  // default az vivid
+                .build();
 
-       return null;
+        ImagePrompt imagePrompt = new ImagePrompt(question.question(), options);
+
+        var imageResponse = imageClient.call(imagePrompt);
+
+       return Base64.getDecoder().decode(imageResponse.getResult().getOutput().getB64Json());
     }
 }
